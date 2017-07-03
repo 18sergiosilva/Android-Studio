68aa63efb250751eefc2efaf2705ab02d67aa3c1
SERGIO <18sergiosilva@gmail.com>
2017-07-03 16:20:09 -0600

    Cuarta Leccion

---
+++ Cuarta Leccion/S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/utilities/detaillActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.sunshine.utilities;
+
+import android.os.Bundle;
+
+/**
+ * Created by elizabeth on 03/07/2017.
+ */
+
+public class detaillActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ Cuarta Leccion/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // TODO (2) Display the weather forecast that was passed from MainActivity
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
+    }
+}
